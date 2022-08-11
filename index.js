712e3ffe44f5096200ca44e661266621b086a879
Neto Kokay <netokokay@gmail.com>
2022-08-11 00:20:56 -0300

    Usando Try Catch

---
+++ index.js
@@ -0,0 +1,7 @@
+try {
+    console.log(test)
+} catch (error){
+    console.log("Opa, isto não devia acontecer, contate o desenvolvedor e informe a referência do erro: ")
+    console.log("")
+    console.log(error)
+}
